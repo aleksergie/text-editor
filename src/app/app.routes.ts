8c99a256dd6e3e7206ace7f80fb2c3fcbd668c20
Oleksii Serhiienko <oleksii.serhiienko@dev.pro>
2025-12-04 21:35:13 +0200

    Added editor lib and component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'editor',
+    loadComponent: () => import('@text-editor/editor').then(m => m.EditorComponent),
+  },
+];
